3223dd68f3516c838af49c2440562ff61f30c299
Varni1102 <varnikumar1512@gmail.com>
2026-02-20 21:00:53 +0530

    solve error

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://kisanhub.vercel.app/'], credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
